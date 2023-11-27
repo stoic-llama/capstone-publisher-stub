b65b3f100e37d0c61c012d0657920651463f98c2
John Liu <stoic.llama@gmail.com>
2023-11-27 03:56:25 -0500

    Test

--- controllers/publisherController.js
+++ controllers/publisherController.js
@@ -106,12 +106,13 @@ const createDevops = function (heartbeat) {
     // Now we are flattening the data structure projects --> builds level.
     Promise.all(
         projects.map((project) => {
+            let api = process.env.JENKINS_API_KEY
             let config = {
                 method: 'get',
                 maxBodyLength: Infinity,
                 url: project.url,
                 headers: {
-                    Authorization: process.env.JENKINS_API_KEY,
+                    Authorization: `Basic ${api}`,
                 },
             };
   
